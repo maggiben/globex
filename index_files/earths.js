d5374f54804e304c359926c756fbe5d9b920f507
Benjamin Maggi <bmaggi@elementum.com>
2017-05-26 18:24:27 -0300

    pin marker

--- index_files/earths.js
+++ index_files/earths.js
@@ -11,16 +11,18 @@ class Maker {
     }
 		this.renderer = this.createRenderer();
     this.universe = this.universe();
-    //this.dish();
-    //this.background();
+    // this.dish();
+    // this.background();
     
     this.planet = new THREE.Group();
     this.planet.add(this.globe());
-    //this.planet.add(this.core());
-    // this.planet.add(this.orbit());
-    // this.planet.add(this.ticks());
+    this.planet.add(this.core());
+    this.planet.add(this.orbit());
+    this.planet.add(this.ticks());
+    // this.planet.add(this.lines());
     this.planet.add(this.rings());
-    // this.planet.add(this.topu({r: 1.03, color: '0x00a2ff'}))
+    this.planet.add(this.marker());
+    this.planet.add(this.topu({r: 1.03, color: '0x00a2ff'}))
 
     this.world = new THREE.Group();
     this.world.add(this.planet);
@@ -92,8 +94,8 @@ class Maker {
   background () {
     const loader = new THREE.TextureLoader();
     let plateMaterial = new THREE.MeshBasicMaterial({
-      //map: loader.load( 'images/background.jpg' ),
-      map: this.makeTexture(),
+      map: loader.load( 'images/background.jpg' ),
+      //map: this.makeTexture(),
       transparent: true,
       blending: THREE.AdditiveBlending,
       opacity: 0
@@ -161,7 +163,7 @@ class Maker {
 
     this.interval = setInterval(() => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      this.doRing (texture, canvas, ctx, canvas.width / 2, canvas.height / 2, radius, 8, 0.5)
+      this.doRing (texture, canvas, ctx, canvas.width / 2, canvas.height / 2, radius, 8, 0.5, 1)
       // move the texture to give the illusion of moving thru the tunnel
       texture.needsUpdate = true;
       radius += 5;
@@ -173,131 +175,115 @@ class Maker {
     return texture;
   }
 
-  doRing (texture, canvas, ctx, x, y, radius, w, opacity) {
+  doRing (texture, canvas, ctx, x, y, radius, w, opacity = 1) {
     ctx.beginPath();
-    //ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
     ctx.fillStyle = 'rgba(0,0,0,0)';
     ctx.fill();
     ctx.lineWidth = w;
-    ctx.strokeStyle = `rgba(255, 255, 0, opacity)`;
+    ctx.strokeStyle = `rgba(255, 255, 0, ${opacity})`;
     ctx.stroke();
-    texture.needsUpdate = true;
   }
 
   makeTextureX (radius, width) {
+    const me = this;
     const canvas = document.createElement('canvas');
     canvas.width = 512;
     canvas.height = 512;
     const ctx = canvas.getContext('2d');
     const texture = new THREE.Texture(canvas);
 
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    /* circle */
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
-    radius = radius || 70;
-    width = width || 50;
-
-    ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'rgba(0,0,0,0)';
-    ctx.fill();
-    ctx.lineWidth = width;
-    ctx.strokeStyle = 'rgba(0,255,250,0.5)';
-    ctx.stroke();
-
-    /* Text */
-    // ctx.font = '120pt Arial';
-    // ctx.textAlign = "center";
-    // ctx.textBaseline = "middle";
-    // ctx.fillStyle = 'rgba(255,0,0,0.5)';  
-    // ctx.fillText(new Date().getTime(), canvas.width / 2, canvas.height / 2);
-    
-    var init = 24;
-    radius = 0;
-
-    this.interval = setInterval(() => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      this.doRing (texture, canvas, ctx, canvas.width / 2, canvas.height / 2, radius, 8, 0.5)
-      // move the texture to give the illusion of moving thru the tunnel
-      //texture.needsUpdate = true;
-      radius += 5;
-      if(radius > 60) {
-        clearInterval(this.interval);
-      }
-    }, 100)
-
-    // new TWEEN.Tween( attributes )
-    //   .delay( 200 )
-    //   .to( { opacity: 0, radius: 60 }, 2000 )
-    //   .easing(TWEEN.Easing.Quartic.Out)
-    //   .onUpdate(function() {
-    //     // console.log(this.opacity, this.radius)
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //     me.doRing (texture, canvas, ctx, canvas.width / 2, canvas.height / 2, this.radius, 8, this.opacity)
-    //     texture.needsUpdate = true;
-    //   })
-    //   .start(); 
+    let attributes = { opacity: 1, radius: 0, border: 8 };
+    new TWEEN.Tween( attributes )
+      .delay( 200 )
+      .to( { opacity: 0, radius: 40, border: 1 }, 1000 )
+      .easing(TWEEN.Easing.Quartic.Out)
+      .onUpdate(function(progress) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        me.doRing (texture, canvas, ctx, canvas.width / 2, canvas.height / 2, this.radius, this.border, this.opacity)
+        texture.needsUpdate = true;
+      })
+      .repeat(Infinity)
+      .start(); 
 
     return texture;
   }
 
+  coordsMove (latitude, longitude, radius) {
+    let curLat = 90 - latitude;
+    let curLong = 180 - longitude;
+    
+    curLong *= Math.PI/180;
+    curLat *= Math.PI/180;
+         
+    const x = radius * Math.cos(curLong) * Math.sin(curLat);
+    const z = radius * Math.sin(curLong) * Math.sin(curLat);
+    const y = radius * Math.cos(curLat); 
+
+    return {x, y, z};
+  }
+
+  marker () {
+    const marker = new THREE.Group();
+    const radius = 400;
+
+    const tickMaterial = new THREE.LineBasicMaterial( { 
+      color: 0xFFFFFF,
+      linewidth: 1, 
+      depthTest: false,  
+      blending: THREE.AdditiveBlending, 
+      transparent : true,
+      opacity: 1
+    });
+    const geometry = new THREE.Geometry();
+    
+    const coordsa = this.coordsMove(-34.603722, -58.381592, radius);
+    geometry.vertices.push( new THREE.Vector3( coordsa.x, coordsa.y, coordsa.z ) );
+    const coordsb = this.coordsMove(-34.603722, -58.381592, radius + 20);
+    geometry.vertices.push( new THREE.Vector3( coordsb.x, coordsb.y, coordsb.z ) );
+    const line = new THREE.Line( geometry, tickMaterial, THREE.LineStrip );
+    marker.add(line);
+
+
+    const sphere = new THREE.SphereGeometry( 3, 32, 32 )
+    const material = new THREE.LineBasicMaterial( { 
+      color: 0xFFFFFF,
+      linewidth: 1, 
+      depthTest: false,  
+      blending: THREE.AdditiveBlending, 
+      transparent : true,
+      opacity: 1
+    });
+
+    const mesh = new THREE.Mesh( sphere, material );
+    mesh.position.set(coordsb.x, coordsb.y, coordsb.z);
+    marker.add(mesh);
+
+    return marker;
+  }
+
   rings () {
     const geometry = new THREE.CircleGeometry( 132, 32 );
-    // const geometry = new THREE.RingGeometry( 132, 10, 32 );
     geometry.applyMatrix( new THREE.Matrix4().makeRotationFromEuler(new THREE.Vector3(Math.PI / 2, Math.PI, 0)));
     const material = new THREE.MeshBasicMaterial({
-      //color: 0xff0000,
       map: this.makeTextureX(),
       side: THREE.DoubleSide,
       blending: THREE.AdditiveBlending, 
       transparent : true,
-      opacity: 1
+      opacity: 1,
       // wireframe: true
     });
 
     const mesh = new THREE.Mesh( geometry, material );
-    //https://threejs.org/docs/#api/core/Geometry
-    let curLat = -32;
-    let curLong = 180 - (-54);
-    curLong *= Math.PI/180;
-    curLat *= Math.PI/180;
 
-    /*
-    new TWEEN.Tween( mesh.rotation)
-      .to( { z: curLat, y: -curLong}, 800)
-      .easing(TWEEN.Easing.Quartic.Out)
-      .start();
-    */
-
-    const altitude = 0;
-    let radius = 330;
-    let latitude =  8//(Math.random() * Math.PI);
-    let longitude = 2//Math.PI - (Math.random() * (2*Math.PI));
-    let x = radius * Math.cos(longitude) * Math.sin(latitude);
-    let y = radius * Math.cos(latitude);    
-    let z = radius * Math.sin(longitude) * Math.sin(latitude);
+    const radius = 422.5;
     
-    console.log(x, y, z)
-    const point = this.mapPoint(34,2);
-    // mesh.position.set(point.x * altitude, point.y * altitude, point.z * altitude);
-    mesh.position.set(z, y, z);
+    const coords = this.coordsMove(-34.603722, -58.381592, radius);
+    mesh.position.set(coords.x, coords.y, coords.z);
     mesh.lookAt(new THREE.Vector3(0,0,0));
 
-    // new TWEEN.Tween( geometry.parameters )
-    //   .delay( 1200 )
-    //   .to( { innerRadius: 1, outerRadius: 10 }, 2000 )
-    //   .easing(TWEEN.Easing.Quartic.Out)
-    //   .start();
-
-    //mesh.geometry.verticesNeedUpdate = true;
-
-    //
-    // uvs.push( new THREE.Vector2( o / thetaSegments, i / phiSegments ) );
-    //
-    console.log(geometry, mesh)
     return mesh;
   }
 
@@ -347,6 +333,39 @@ class Maker {
       .start();
     
     return coreParticles;
+  }
+
+  lines () {
+    const lineGeometry = new THREE.Geometry();
+    const lineRadius = 480;
+    const loader = new THREE.TextureLoader();
+
+    for (var longitude = 2*Math.PI; longitude >= 0; longitude-=Math.PI/12) {
+      for (var latitude= 0; latitude <= Math.PI; latitude+=Math.PI/30) {
+        const x = lineRadius * Math.cos(longitude) * Math.sin(latitude);
+        const z = lineRadius * Math.sin(longitude) * Math.sin(latitude);
+        const y = lineRadius * Math.cos(latitude);                        
+
+        const vector = new THREE.Vector3( x, y, z );
+        lineGeometry.vertices.push(vector);                        
+      }   
+    }           
+
+    const lineMaterial = new THREE.PointsMaterial( { 
+      size: 12,
+      map: loader.load( "textures/sprites/circle.png" ) , 
+      depthTest: false,  
+      blending: THREE.AdditiveBlending, 
+      transparent : true 
+    });
+
+    lineMaterial.color.setHSL( .55, .45, 1.4 );
+
+    const lineParticles = new THREE.Points( lineGeometry, lineMaterial );
+    //lineParticles.sortParticles = true;
+    lineParticles.updateMatrix();  
+
+    return lineParticles;
   }
 
   ticks () {
@@ -494,9 +513,11 @@ class Maker {
       var yIndex = 0;
       for (let latitude= 0; latitude <= Math.PI; latitude+=Math.PI/(240)) {
         if (mapData[yIndex][xIndex] == 0) {
+          
           const x = 400 * Math.cos(longitude) * Math.sin(latitude);
           const z = 400 * Math.sin(longitude) * Math.sin(latitude);
-          const y = 400 * Math.cos(latitude);                       
+          const y = 400 * Math.cos(latitude);
+
           tgeometry.vertices.push(new THREE.Vector3(x, y, z));
           earthColors[ colorIndex ] = new THREE.Color( 0x1b9ebc );
           //earthColors[ colorIndex ].setHSL( .12, 0, .5 ); 
